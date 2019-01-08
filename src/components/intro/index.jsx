6d382df48e6390bbd50f1d9a155c9a14b981897d
Gustav Pursche <pursche@posteo.de>
2019-01-08 16:05:43 +0100

    Add page Ausfuehrung

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -4,10 +4,12 @@ import Constraint from '../constraint';
 
 import './style.scss';
 
-export default ({ children, title }) => (
+export default ({ children, title, intro }) => (
   <section className="intro">
     <Constraint>
       <h1 className="intro__title">{title}</h1>
+
+      {intro && <p className="intro__text">{intro}</p>}
 
       {children}
     </Constraint>
---
+++ src/pages/ausfuehrung/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Intro from '../../components/intro';
+import withLayout from '../../components/with-layout';
+
+import './style.scss';
+
+export default withLayout('Ausfuehrung', () => (
+  <div className="ausfuehrung">
+    <Intro
+      title="Checklist"
+      intro="Ein Bürgerbegehren besteht aus lauter kleinen Schritten. Zunächst verschaffen wir dem Thema eine Öffentlichkeit. Dann zwingen wir die lokale Politik, Farbe zu bekennen und sich zur Klimawende zu positionieren. Wenn sie sich weigert, erhöhen wir den Druck – und führen die Entscheidung irgendwann selbst herbei. Jede dieser Etappen ist für sich genommen schon ein Sieg. Eine Bürgerbegehrens-Kampagne kann also nicht wirklich scheitern. Die Frage ist vielmehr, wie viel sie gewinnt."
+    />
+  </div>
+));
