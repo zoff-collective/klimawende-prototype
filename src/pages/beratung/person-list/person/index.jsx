84d2fe27563c0a98155044bc2299519b383edb1e
Gustav Pursche <pursche@posteo.de>
2019-01-15 09:57:46 +0100

    Improve font-sizes on small screens

--- src/pages/beratung/person-list/person/index.jsx
+++ src/pages/beratung/person-list/person/index.jsx
@@ -7,7 +7,7 @@ import './style.scss';
 
 export default ({ name, email, intro, phone, image }) => (
   <li className="person-list__person">
-    <img src={image} alt="" />
+    <img src={image} alt="" className="person-list__person-image" />
     <div className="person-list__person-content">
       <Headline level="3" levelStyle="3" underlined>
         {name}
