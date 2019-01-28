c9687a8671c1b3eb19ca5783985ec118159bc64b
Gustav Pursche <pursche@posteo.de>
2019-01-28 15:44:21 +0100

    Person: add email and phone icon

--- src/components/person/index.jsx
+++ src/components/person/index.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import Headline from '../text/headline';
 import Paragraph from '../text/paragraph';
 
+import EnvelopeIcon from '../../../static/icons/envelope.svg';
+import PhoneIcon from '../../../static/icons/phone.svg';
+
 import './style.scss';
 
 export default ({ name, email, intro, phone, image }) => (
@@ -15,9 +18,19 @@ export default ({ name, email, intro, phone, image }) => (
 
       <Paragraph text={intro} />
 
-      {email && <p className="person__email">{email}</p>}
+      {email && (
+        <p className="person__meta">
+          <EnvelopeIcon className="person__meta-icon" />
+          {email}
+        </p>
+      )}
 
-      {phone && <p className="person__phone">{phone}</p>}
+      {phone && (
+        <p className="person__meta">
+          <PhoneIcon className="person__meta-icon" />
+          {phone}
+        </p>
+      )}
     </div>
   </div>
 );
