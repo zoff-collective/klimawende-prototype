8654586c3369cc8c9bc4e3fd2ae6ca0f53305da2
Gustav Pursche <pursche@posteo.de>
2019-02-14 13:51:53 +0100

    Add empty overview pages for Erfolge and So gehts

--- src/pages/erfolge/index.jsx
+++ src/pages/erfolge/index.jsx
@@ -1,3 +1,17 @@
 import React from 'react';
 
-export default () => <div />;
+import Intro from '../../components/intro';
+import withLayout from '../../components/with-layout';
+
+import './style.scss';
+
+const Page = () => (
+  <div className="erfolge">
+    <Intro
+      title="Erfolge"
+      intro="Ein Bürgerbegehren besteht aus lauter kleinen Schritten. Zunächst verschaffen wir dem Thema eine Öffentlichkeit. Dann zwingen wir die lokale Politik, Farbe zu bekennen und sich zur Klimawende zu positionieren. Wenn sie sich weigert, erhöhen wir den Druck – und führen die Entscheidung irgendwann selbst herbei. Jede dieser Etappen ist für sich genommen schon ein Sieg. Eine Bürgerbegehrens-Kampagne kann also nicht wirklich scheitern. Die Frage ist vielmehr, wie viel sie gewinnt."
+    />
+  </div>
+);
+
+export default withLayout('Erfolge', Page);
--- src/pages/so-gehts/index.jsx
+++ src/pages/so-gehts/index.jsx
@@ -1,3 +1,15 @@
 import React from 'react';
 
-export default () => <div />;
+import Intro from '../../components/intro';
+import withLayout from '../../components/with-layout';
+
+const Page = () => (
+  <div className="so-gehts">
+    <Intro
+      title="So geht's"
+      intro="Ein Bürgerbegehren besteht aus lauter kleinen Schritten. Zunächst verschaffen wir dem Thema eine Öffentlichkeit. Dann zwingen wir die lokale Politik, Farbe zu bekennen und sich zur Klimawende zu positionieren. Wenn sie sich weigert, erhöhen wir den Druck – und führen die Entscheidung irgendwann selbst herbei. Jede dieser Etappen ist für sich genommen schon ein Sieg. Eine Bürgerbegehrens-Kampagne kann also nicht wirklich scheitern. Die Frage ist vielmehr, wie viel sie gewinnt."
+    />
+  </div>
+);
+
+export default withLayout("So geht's", Page);
