58d2cf400b09f35a32163037327bd554b80aaacc
Gustav Pursche <pursche@posteo.de>
2019-02-27 11:58:42 +0100

    Select: close element, when click is outside and the select is open

--- static/javascript/super-select.js
+++ static/javascript/super-select.js
@@ -3,7 +3,25 @@ const OPEN_CLASSNAME = 'super-select--is-open';
 const isOpen = select => select.classList.contains(OPEN_CLASSNAME);
 
 const toggleSelect = select => {
-  select.classList.toggle(OPEN_CLASSNAME, !isOpen(select));
+  const selectIsOpen = isOpen(select);
+
+  const closeSelectOnClickOutside = event => {
+    const clickInside = select.contains(event.target);
+
+    if (!clickInside) {
+      select.classList.remove(OPEN_CLASSNAME);
+
+      // remove original event listener
+      document.removeEventListener('click', closeSelectOnClickOutside);
+    }
+  };
+
+  // close select when click is outside of the element
+  if (!selectIsOpen) {
+    document.addEventListener('click', closeSelectOnClickOutside);
+  }
+
+  select.classList.toggle(OPEN_CLASSNAME, !selectIsOpen);
 };
 
 const init = select => {
