4060462dc9bbe2c05136b74811a5120a7dd8c383
Gustav Pursche <pursche@posteo.de>
2019-03-06 17:56:39 +0100

    Unify two-column layout into mixin

--- src/pages/so-gehts/direkte-demokratie-verstehen/index.jsx
+++ src/pages/so-gehts/direkte-demokratie-verstehen/index.jsx
@@ -1,18 +1,42 @@
 import { graphql } from 'gatsby';
 import React from 'react';
 
+import BlockContent from '../../../components/block-content';
+import Button from '../../../components/form/button';
+import Constraint from '../../../components/constraint';
 import Intro from '../../../components/intro';
+import Participate from '../../../components/participate';
 import withLayout from '../../../components/with-layout';
 
 import '../style.scss';
 
 const Page = ({
   data: {
-    content: { title, intro, image }
+    content: { title, intro, image, blocks }
   }
 }) => (
   <div className="so-gehts">
     <Intro title={title} intro={intro} image={image} />
+
+    <section className="so-gehts__body">
+      <Constraint width="wide" className="so-gehts__body-constraint">
+        <div className="so-gehts__body-content-container">
+          <BlockContent blocks={blocks} />
+        </div>
+
+        <div className="so-gehts__body-action-container">
+          <Participate
+            title="Wir beraten Dich!"
+            intro="Du möchtest an diesem Standort ein Bürgerbegehren starten, hast aber viele offenen Fragen?"
+            share={false}
+          >
+            <Button theme="current" fullWidth>
+              Zur Beratung
+            </Button>
+          </Participate>
+        </div>
+      </Constraint>
+    </section>
   </div>
 );
 
@@ -27,6 +51,10 @@ export const query = graphql`
       image {
         src
         alt
+      }
+      blocks {
+        type
+        content
       }
     }
   }
